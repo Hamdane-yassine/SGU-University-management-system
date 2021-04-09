19d68ac1e9761bfe8ccd8cea620959f54a656451
Hamdane-yassine <hamdaneyassine10@gmail.com>
2021-04-09 16:15:27 +0100

    auth

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
