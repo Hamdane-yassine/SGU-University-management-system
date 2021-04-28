4c8e2de6b6433b062a7c291d84c2e8dc4d635762
Mehdi <mehdi.elg@gmail.com>
2021-04-28 14:09:50 +0000

    g

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,19 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Truncate all tables, except migrations
+        // +++++++++++ START +++++++++++++
+        Schema::disableForeignKeyConstraints();
+        $tables = DB::select('SHOW TABLES');
+        foreach ($tables as $table) {
+            if ($table->Tables_in_pfe !== 'migrations')
+                DB::table($table->Tables_in_pfe)->truncate();
+        }
+        // +++++++++++ END +++++++++++++++=
+
         // \App\Models\User::factory()->count(2)->create();
         // \App\Models\User::factory()->create([
-        //         'email' => 'fbed1af31d-199be7@inbox.mailtrap.io'
-        // ]);
+            //         'email' => 'fbed1af31d-199be7@inbox.mailtrap.io'
+            // ]);
 
         // \App\Models\Personne::factory()->has(\App\Models\User::factory()->create())->create();
-        // \App\Models\User::factory()->create();
-        // \App\Models\Personne::factory()->create();
+        \App\Models\User::factory()->count(10)->create();
 
 
         // =======================================
 
-        // \App\Models\Etudiant::factory()->count(2)->create();
-        \App\Models\Professeur::factory()->count(2)->create();
-    }
+        }
+
+
 }
