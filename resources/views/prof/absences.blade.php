e1047c6592dc5f3a68d792fc3d4125ed27320887
Hamdane-yassine <hamdaneyassine10@gmail.com>
2021-04-28 12:00:52 +0000

    InstallLayoutToViews

--- resources/views/prof/absences.blade.php
+++ resources/views/prof/absences.blade.php
@@ -99,12 +99,12 @@
     <!-- js -->
     @endsection
     @section('SpecialScripts')
-    <script src="src/plugins/apexcharts/apexcharts.min.js"></script>
-    <script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
-    <script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
-    <script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
-    <script src="vendors/scripts/dashboard3.js"></script>
-    <script src="src/plugins/jquery-steps/jquery.steps.js"></script>
-    <script src="vendors/scripts/steps-setting.js"></script>
+    <script src="{{ asset('src/plugins/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('src/plugins/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('src/plugins/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('src/plugins/datatables/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('src/plugins/datatables/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/scripts/dashboard3.js') }}"></script>
+    <script src="{{ asset('src/plugins/jquery-steps/jquery.steps.js') }}"></script>
+    <script src="{{ asset('vendors/scripts/steps-setting.js') }}"></script>
     @endsection
