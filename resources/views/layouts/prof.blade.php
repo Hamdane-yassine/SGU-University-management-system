4c9b5bc929b56cd29221fcebd96fe3564b32979d
Hamdane-yassine <hamdaneyassine10@gmail.com>
2021-04-29 14:10:47 +0000

    logout

--- resources/views/layouts/prof.blade.php
+++ resources/views/layouts/prof.blade.php
@@ -88,7 +88,10 @@
                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
                     <a class="dropdown-item" href="profile.html"><i class="dw dw-user1"></i> Profile</a>
                     <a class="dropdown-item" href="faq.html"><i class="dw dw-help"></i> Aide</a>
-                    <a class="dropdown-item" href="login.html"><i class="dw dw-logout"></i> Se déconnecter</a>
+                    <a class="dropdown-item"  href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="dw dw-logout"></i> Se déconnecter</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
