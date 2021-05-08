e5208db760f0ae436d07cdcd2b17079713230dc8
OtmanDaoudi <daoudiotman22@gmail.com>
2021-05-08 15:54:03 +0000

    'done working on sending mails to student upon filiere upload success'

---
+++ app/Jobs/SendEmploiEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\EmploiMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmploiEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $mailData;
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->mailData['mailTo'])->send(new EmploiMail($this->mailData['userName'],$this->mailData['nomfiliere'],
+        $this->mailData['filePath']));
+    }
+}
