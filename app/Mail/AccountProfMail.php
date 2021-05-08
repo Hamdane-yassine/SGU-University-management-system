cf299f5fd0d62c55bfb39351d4620d3649711106
Hamdane-yassine <hamdaneyassine10@gmail.com>
2021-05-08 15:30:25 +0000

    zss

---
+++ app/Mail/AccountProfMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AccountProfMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    protected $userNam;
+    protected $matiereName;
+    protected $profName;
+    protected $absenceDate;
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
