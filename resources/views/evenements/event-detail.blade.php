d6f04da4ba6a7963693b1d402ea077607a571f72
Mehdi <mehdi.elg@gmail.com>
2021-05-11 13:49:51 +0000

    a

--- resources/views/evenements/event-detail.blade.php
+++ resources/views/evenements/event-detail.blade.php
@@ -28,6 +28,21 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-md-8 col-sm-12">
+                                <div class=" overflow-hidden mb-30">
+                                    @if($attachments)
+                                    <form method="GET" action="{{ route('evenement.download', $evenement->idEvenement) }}">
+                                        <button type="submit" id="download" type="button" class="btn btn-success">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
+                                                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
+                                            </svg>
+                                            Telecharger les fichers
+                                        </button>
+                                    </form>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <div class="card-box mb-30">
@@ -44,27 +59,6 @@
                                         </ul>
                                     </div>
                                 @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="blog-wrap">
-                <div class="container pd-1">
-                    <div class="row">
-                        <div class="col-md-8 col-sm-12">
-                            <div class=" overflow-hidden mb-30">
-                                @if($attachments)
-                                <form method="GET" action="{{ route('evenement.download', $evenement->idEvenement) }}">
-                                    <button type="submit" id="download" type="button" class="btn btn-success">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
-                                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-                                        </svg>
-                                        Telecharger les fichers
-                                    </button>
-                                </form>
-                                @endif
                             </div>
                         </div>
                     </div>
