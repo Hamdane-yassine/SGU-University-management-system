18a80f8902e6bb7d76a09e051c26045566a56db4
Mehdi <mehdi.elg@gmail.com>
2021-05-18 15:57:52 +0100

    heroku

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://cryptic-ocean-39951.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
