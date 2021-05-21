b313a646f5f8422ac09759440a9dbe069fc934d0
Hamdane-yassine <hamdaneyassine10@gmail.com>
2021-05-21 17:03:47 +0100

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,3 @@
 <div class="footer-wrap pd-20 mb-20 card-box">
-<<<<<<< HEAD
    Réalisé pour le projet de fin d’étude diplôme DUT 2020/2021
-=======
-<<<<<<< HEAD
-   Réalisé pour le projet de fin d’étude diplôme DUT 2020/2021
-=======
-   Réalisé pour le projet de fin d’étude diplôme DUT 2020/2021</a>
->>>>>>> 473d238861b8571b57fb3e0c887d299aa5d65638
->>>>>>> 113fe4fd6dda781a923529c093c141a566cc9610
 </div>
