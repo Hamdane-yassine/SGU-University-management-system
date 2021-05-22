022e5d3ff6d28b35b7865493208523a6343b1b49
Mehdi <mehdi.elg@gmail.com>
2021-05-22 22:19:35 +0100

    info message if there is none

--- resources/views/components/EventsComponents.blade.php
+++ resources/views/components/EventsComponents.blade.php
@@ -1,3 +1,4 @@
+@if(\App\Models\Evenement::orderBy('created_at','DESC')->count())
 @foreach (\App\Models\Evenement::orderBy('created_at','DESC')->take(5)->get() as $evt)
     <li>
         <a href="{{ url('/evenement/'.$evt->idEvenement) }}">{{ $evt->titre }}
@@ -7,3 +8,10 @@
         </a>
     </li>
 @endforeach
+@else
+<div class="chat-body clearfix ml-3">
+    <p style="word-wrap: break-word"><span class="d-block text-secondary pb-1">
+    </span class="align-self-center" ><i class="fa fa-info-circle mr-1"></i>Aucun événement n'est trouvé pour le moment</span>
+    <div class="chat_time float-right mb-10"> </div>
+</div>
+@endif
