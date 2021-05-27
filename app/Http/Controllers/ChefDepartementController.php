408b38c4d88728c0396ecfecd015fd30e50c6575
Mehdi <mehdi.elg@gmail.com>
2021-05-28 00:30:30 +0100

    '

--- app/Http/Controllers/ChefDepartementController.php
+++ app/Http/Controllers/ChefDepartementController.php
@@ -499,7 +499,7 @@ class ChefDepartementController extends Controller
             ->join('filiere', 'semestre.idFiliere', '=', 'filiere.idFiliere')
             ->join('users', 'users.id', '=', 'professeur.idUtilisateur')
             ->join('personne', 'personne.idPersonne', 'users.idPersonne')
-            ->select('Absence.idAbsence as idAbsence', 'personne.nom as nomProf', 'Absence.dateAbsence as dateAbsence')
+            ->select('Absence.idAbsence', 'personne.nom', 'Absence.dateAbsence')
             ->get();
 
         if ($request->ajax()) {
